1c9cb017217644cdfb7fde3ab43008ad121a48fd
davezer <davezer27@gmail.com>
2021-12-15 18:17:39 -0500

    added comment API and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes.js');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -24,6 +24,15 @@ router.get('/:id', (req, res) => {
         model: Post,
         attributes: ['id', 'title', 'post_url', 'created_at']
       },
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
       {
         model: Post,
         attributes: ['title'],
@@ -32,6 +41,7 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
+  
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
